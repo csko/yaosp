210f4e5abfcf3dedd04efe9aa950e0465174058d
csko <csko@csko.hu>
2009-01-21 15:49:36 +0000

    Missing file from last commit.

---
+++ source/lib/c/src/sys/stime.c
@@ -0,0 +1,26 @@
+/* stime function
+ *
+ * Copyright (c) 2009 Kornel Csernai
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <sys/time.h>
+
+#include <yaosp/syscall.h>
+#include <yaosp/syscall_table.h>
+
+int stime( time_t *t ) {
+    return syscall1( SYS_stime, ( int )t );
+}
