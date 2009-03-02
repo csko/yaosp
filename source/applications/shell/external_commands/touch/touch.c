10206327a3c71eb18621f232e2dbe4ead2ad97ae
Zoltan Kovacs <giszo.k@gmail.com>
2009-03-02 20:43:23 +0000

    Coding style cleanup #2

--- source/applications/shell/external_commands/touch/touch.c
+++ source/applications/shell/external_commands/touch/touch.c
@@ -22,37 +22,39 @@
 #include <unistd.h>
 #include <getopt.h>
 
-#define TOUCH_ATIME 1
-#define TOUCH_NOCREATE 2
-#define TOUCH_MTIME 4
+enum {
+    TOUCH_ATIME = ( 1 << 0 ),
+    TOUCH_NOCREATE = ( 1 << 1 ),
+    TOUCH_MTIME = ( 1 << 2 )
+};
 
-static char* argv0 = NULL;
+static char* argv0;
 
 static void print_usage( int status ) {
     if ( status != EXIT_SUCCESS ) { /* option error */
         fprintf(stderr, "Try `%s --help' for more information.\n", argv0);
     } else { /* --help */
-        printf("Usage: %s [OPTION]... FILE...\n", argv0);
-        printf("Update the access and modification times of each FILE to the current time.\n\
-\n\
-A FILE argument that does not exist is created empty.\n\
-\n\
-A FILE argument string of - is handled specially and causes touch to\n\
-change the times of the file associated with standard output.\n\
-\n\
-Mandatory arguments to long options are mandatory for short options too.\n\
-      -a                     change only the access time\n\
-      -c, --no-create        do not create any files\n\
-      -d, --date=STRING      parse STRING and use it instead of current time\n\
-      -m                     change only the modification time\n\
-      -r, --reference=FILE   use this file's times instead of current time\n\
-      -t STAMP               use [[CC]YY]MMDDhhmm[.ss] instead of current time\n\
-      --time=WORD            change the specified time:\n\
-                               WORD is access, atime, or use: equivalent to -a\n\
-                               WORD is modify or mtime: equivalent to -m\n\
-      --help                 display this help and exit\n\
-\n\
-Note that the -d and -t options accept different time-date formats.\n");
+        printf( "Usage: %s [OPTION]... FILE...\n", argv0 );
+        printf( "Update the access and modification times of each FILE to the current time.\n" );
+        printf( "\n" );
+        printf( "A FILE argument that does not exist is created empty.\n" );
+        printf( "\n" );
+        printf( "A FILE argument string of - is handled specially and causes touch to\n" );
+        printf( "change the times of the file associated with standard output.\n" );
+        printf( "\n" );
+        printf( "Mandatory arguments to long options are mandatory for short options too.\n" );
+        printf( "      -a                     change only the access time\n" );
+        printf( "      -c, --no-create        do not create any files\n" );
+        printf( "      -d, --date=STRING      parse STRING and use it instead of current time\n" );
+        printf( "      -m                     change only the modification time\n" );
+        printf( "      -r, --reference=FILE   use this file's times instead of current time\n" );
+        printf( "      -t STAMP               use [[CC]YY]MMDDhhmm[.ss] instead of current time\n" );
+        printf( "      --time=WORD            change the specified time:\n" );
+        printf( "                               WORD is access, atime, or use: equivalent to -a\n" );
+        printf( "                               WORD is modify or mtime: equivalent to -m\n" );
+        printf( "      --help                 display this help and exit\n" );
+        printf( "n" );
+        printf( "Note that the -d and -t options accept different time-date formats.\n" );
     }
 
     exit( status );
@@ -61,16 +63,16 @@ Note that the -d and -t options accept different time-date formats.\n");
 static char const short_options[] = "acd:t:h";
 
 static struct option long_options[] = {
-    {"no-create", no_argument, NULL, 'c'},
-    {"date", required_argument, NULL, 'd'},
-    {"reference", required_argument, NULL, 'r'},
-    {"time", required_argument, NULL, 'U'},
-    {"help", no_argument, NULL, 'h'},
-/*    {"version", no_argument, NULL, 'V'}, */
-    {NULL, 0, NULL, 0}
+    { "no-create", no_argument, NULL, 'c' },
+    { "date", required_argument, NULL, 'd' },
+    { "reference", required_argument, NULL, 'r' },
+    { "time", required_argument, NULL, 'U' },
+    { "help", no_argument, NULL, 'h' },
+    /*{ "version", no_argument, NULL, 'V' },*/
+    { NULL, 0, NULL, 0 }
 };
 
-int do_touch(const char* file, int mode){
+int do_touch( const char* file, int mode ) {
     int fd;
 
     /* TODO: check mode */
@@ -98,42 +100,55 @@ int main( int argc, char** argv ) {
     /* Get the command line options */
     opterr = 0;
 
-    for(;;){
-        optc = getopt_long (argc, argv, short_options,
-                       long_options, NULL);
+    for ( ;; ) {
+        optc = getopt_long(
+            argc,
+            argv,
+            short_options,
+            long_options,
+            NULL
+        );
 
-        if (optc == -1){ /* No more options */
+        if ( optc == -1 ) {
+            /* No more options */
             break;
         }
 
         /* TODO */
-        switch(optc){
+        switch ( optc ) {
             case 'a':
                 mode |= TOUCH_ATIME;
                 break;
+
             case 'c':
                 mode |= TOUCH_NOCREATE;
                 break;
+
             case 'd':
                 mode |= TOUCH_MTIME;
                 break;
+
             case 'r':
                 break;
+
             case 't':
                 break;
+
             case 'h':
-                print_usage(EXIT_SUCCESS);
+                print_usage( EXIT_SUCCESS );
                 break;
+
             /* TODO */
             default:
-                print_usage(EXIT_FAILURE);
+                print_usage( EXIT_FAILURE );
+                break;
         }
     }
 
     /* TODO: checks */
-    if (optind == argc){
-        fprintf(stderr, "%s: missing operand\n", argv0);
-        print_usage(EXIT_FAILURE);
+    if ( optind == argc ) {
+        fprintf( stderr, "%s: missing operand\n", argv0 );
+        print_usage( EXIT_FAILURE );
     }
 
     for ( i = optind; i < argc; i++ ) {
@@ -141,7 +156,6 @@ int main( int argc, char** argv ) {
             error = EXIT_FAILURE;
         }
     }
-
 
     return error;
 }
