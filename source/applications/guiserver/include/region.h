ead11558c43012199e7d27bc436aca9a233c90e9
Zoltan Kovacs <giszo.k@gmail.com>
2009-04-02 20:08:01 +0000

    Started to implement dirty/visible region handling for windows

---
+++ source/applications/guiserver/include/region.h
@@ -0,0 +1,38 @@
+/* GUI server
+ *
+ * Copyright (c) 2009 Zoltan Kovacs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _REGION_H_
+#define _REGION_H_
+
+#include <ygui/rect.h>
+
+typedef struct clip_rect {
+    rect_t rect;
+    struct clip_rect* next;
+} clip_rect_t;
+
+typedef struct region {
+    clip_rect_t* rects;
+} region_t;
+
+int init_region( region_t* region );
+int destroy_region( region_t* region );
+
+int init_region_manager( void );
+
+#endif /* _REGION_H_ */
---
+++ source/applications/guiserver/src/region.c
@@ -0,0 +1,124 @@
+/* GUI server
+ *
+ * Copyright (c) 2009 Zoltan Kovacs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <stdlib.h>
+#include <assert.h>
+#include <errno.h>
+#include <yaosp/semaphore.h>
+
+#include <region.h>
+
+#define MAX_FREE_CLIP_RECT     64
+#define INITIAL_FREE_CLIP_RECT 32
+
+static int free_clip_rect_count = 0;
+static semaphore_id clip_rect_lock = -1;
+static clip_rect_t* clip_rect_list = NULL;
+
+static clip_rect_t* get_clip_rect( void ) {
+    clip_rect_t* rect;
+
+    rect = NULL;
+
+    LOCK( clip_rect_lock );
+
+    if ( free_clip_rect_count > 0 ) {
+        assert( clip_rect_list != NULL );
+
+        rect = clip_rect_list;
+        clip_rect_list = rect->next;
+
+        free_clip_rect_count--;
+    }
+
+    UNLOCK( clip_rect_lock );
+
+    if ( rect == NULL ) {
+        rect = ( clip_rect_t* )malloc( sizeof( clip_rect_t ) );
+
+        if ( rect == NULL ) {
+            return NULL;
+        }
+    }
+
+    rect->next = NULL;
+
+    return rect;
+}
+
+static void put_clip_rect( clip_rect_t* rect ) {
+    LOCK( clip_rect_lock );
+
+    if ( free_clip_rect_count < MAX_FREE_CLIP_RECT ) {
+        rect->next = clip_rect_list;
+        clip_rect_list = rect;
+
+        free_clip_rect_count++;
+
+        rect = NULL;
+    }
+
+    UNLOCK( clip_rect_lock );
+
+    if ( rect != NULL ) {
+        free( rect );
+    }
+}
+
+int init_region( region_t* region ) {
+    region->rects = NULL;
+
+    return 0;
+}
+
+int destroy_region( region_t* region ) {
+    clip_rect_t* rect;
+
+    while ( region->rects != NULL ) {
+        rect = region->rects;
+        region->rects = rect->next;
+
+        put_clip_rect( rect );
+    }
+
+    return 0;
+}
+
+int init_region_manager( void ) {
+    int i;
+    clip_rect_t* rect;
+
+    for ( i = 0; i < INITIAL_FREE_CLIP_RECT; i++ ) {
+        rect = ( clip_rect_t* )malloc( sizeof( clip_rect_t ) );
+
+        if ( rect == NULL ) {
+            return -ENOMEM;
+        }
+
+        rect->next = clip_rect_list;
+        clip_rect_list = rect;
+    }
+
+    clip_rect_lock = create_semaphore( "clip rect list lock", SEMAPHORE_BINARY, 0, 1 );
+
+    if ( clip_rect_lock < 0 ) {
+        return clip_rect_lock;
+    }
+
+    return 0;
+}
