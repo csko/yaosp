af238b72f897a9db62246fd0321eac7426ccd63c
Zoltan Kovacs <giszo.k@gmail.com>
2009-11-17 15:30:51 +0000

    Removed some dead code from kernel/src/linker/elf.c file.

--- source/kernel/src/linker/elf.c
+++ source/kernel/src/linker/elf.c
@@ -27,27 +27,4 @@ bool elf_check_header( elf_header_t* header, uint8_t class, uint8_t data, uint16
              ( header->ident[ ID_DATA ] == data ) &&
              ( header->type == type ) &&
              ( header->machine == machine ) );
-
-#if 0
-    if ( ( header->ident[ ID_MAGIC0 ] != ELF32_MAGIC0 ) ||
-         ( header->ident[ ID_MAGIC1 ] != ELF32_MAGIC1 ) ||
-         ( header->ident[ ID_MAGIC2 ] != ELF32_MAGIC2 ) ||
-         ( header->ident[ ID_MAGIC3 ] != ELF32_MAGIC3 ) ) {
-        return false;
-    }
-
-    if ( header->ident[ ID_CLASS ] != ELF_CLASS_32 ) {
-        return false;
-    }
-
-    if ( header->ident[ ID_DATA ] != ELF_DATA_2_LSB ) {
-        return false;
-    }
-
-    if ( header->machine != ELF_EM_386 ) {
-        return false;
-    }
-
-    return true;
-#endif
 }
