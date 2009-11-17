f20a1a7d97ab71e96706a804c3049871c1b17140
Zoltan Kovacs <giszo.k@gmail.com>
2009-11-17 10:55:55 +0000

    Modified the VESA driver to work with the new virtual memory manager.

--- source/drivers/graphics/vesa/vesa.c
+++ source/drivers/graphics/vesa/vesa.c
@@ -37,14 +37,22 @@ static int detect_vesa( void ) {
     int error;
     bios_regs_t regs;
     vesa_info_t* vesa_info;
+    memory_region_t* info_region;
 
     uint16_t* tmp;
+    ptr_t mode_addr;
     uint16_t* mode_ptr;
 
-    vesa_info = ( vesa_info_t* )0x2000;
+    info_region = memory_region_create( "VESA info", PAGE_SIZE, REGION_KERNEL | REGION_READ | REGION_WRITE );
+
+    if ( info_region == NULL ) {
+        return -ENOSPC;
+    }
+
+    memory_region_remap_pages( info_region, 0x2000 );
+    vesa_info = ( vesa_info_t* )info_region->address;
 
     memset( &regs, 0, sizeof( bios_regs_t ) );
-
     memset( vesa_info, 0, sizeof( vesa_info_t ) );
     memcpy( vesa_info->signature, "VESA", 4 );
 
@@ -55,22 +63,35 @@ static int detect_vesa( void ) {
     error = call_bios_interrupt( 0x10, &regs );
 
     if ( error < 0 ) {
-        return error;
+        goto out;
     }
 
     if ( ( ( regs.ax & 0xFF ) != 0x4F ) ||
          ( ( regs.ax >> 8 ) != 0x00 ) ) {
-        return -ENOENT;
+        error = -ENOENT;
+        goto out;
     }
 
     if ( vesa_info->video_mode_ptr == 0x0000 ) {
-        return -ENOENT;
+        error = -ENOENT;
+        goto out;
     }
 
     kprintf( INFO, "Detected VESA %d.%d compatible graphics card.\n", vesa_info->version >> 8, vesa_info->version & 0xF );
 
-    mode_ptr = ( uint16_t* )( ( ( vesa_info->video_mode_ptr & 0xFFFF0000 ) >> 12 ) |
-                              ( vesa_info->video_mode_ptr & 0xFFFF ) );
+    mode_addr = ( ptr_t )( ( ( vesa_info->video_mode_ptr & 0xFFFF0000 ) >> 12 ) |
+                           ( vesa_info->video_mode_ptr & 0xFFFF ) );
+
+    memory_region_put( info_region );
+
+    info_region = memory_region_create( "VESA mode table", PAGE_SIZE, REGION_KERNEL | REGION_READ );
+
+    if ( info_region == NULL ) {
+        return -1;
+    }
+
+    memory_region_remap_pages( info_region, mode_addr & PAGE_MASK );
+    mode_ptr = ( uint16_t* )( info_region->address + ( mode_addr & ~PAGE_MASK ) );
 
     tmp = mode_ptr;
 
@@ -79,29 +100,42 @@ static int detect_vesa( void ) {
     mode_count = ( ( uint32_t )tmp - ( uint32_t )mode_ptr ) / sizeof( uint16_t );
 
     if ( mode_count == 0 ) {
-        return 0;
+        error = 0;
+        goto out;
     }
 
     mode_table = ( uint16_t* )kmalloc( sizeof( uint16_t ) * mode_count );
 
     if ( mode_table == NULL ) {
-        return -ENOMEM;
+        error = -ENOMEM;
+        goto out;
     }
 
     memcpy( mode_table, mode_ptr, sizeof( uint16_t ) * mode_count );
+    error = 0;
 
-    return 0;
+ out:
+    memory_region_put( info_region );
+
+    return error;
 }
 
 static int vesa_get_mode_information( uint16_t mode_number, vesa_mode_info_t* mode_info ) {
     int error;
     bios_regs_t regs;
+    memory_region_t* info_region;
     vesa_mode_info_t* vesa_mode_info;
 
-    vesa_mode_info = ( vesa_mode_info_t* )0x2000;
+    info_region = memory_region_create( "VESA mode info", PAGE_SIZE, REGION_KERNEL | REGION_READ | REGION_WRITE );
+
+    if ( info_region == NULL ) {
+        return -ENOSPC;
+    }
+
+    memory_region_remap_pages( info_region, 0x2000 );
+    vesa_mode_info = ( vesa_mode_info_t* )info_region->address;
 
     memset( &regs, 0, sizeof( bios_regs_t ) );
-
     memset( vesa_mode_info, 0, sizeof( vesa_mode_info_t ) );
 
     regs.ax = 0x4F01;
@@ -112,17 +146,22 @@ static int vesa_get_mode_information( uint16_t mode_number, vesa_mode_info_t* mo
     error = call_bios_interrupt( 0x10, &regs );
 
     if ( error < 0 ) {
-        return error;
+        goto out;
     }
 
     if ( ( ( regs.ax & 0xFF ) != 0x4F ) ||
          ( ( regs.ax >> 8 ) != 0x00 ) ) {
-        return -ENOENT;
+        error = -ENOENT;
+        goto out;
     }
 
-    memcpy( mode_info, ( void* )0x2000, sizeof( vesa_mode_info_t ) );
+    memcpy( mode_info, vesa_mode_info, sizeof( vesa_mode_info_t ) );
+    error = 0;
 
-    return 0;
+ out:
+    memory_region_put( info_region );
+
+    return error;
 }
 
 static int vesa_set_mode( uint16_t mode_number ) {
