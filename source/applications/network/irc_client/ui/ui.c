92a8d12c43f87a0df4d36c2a5531e80c45195213
Zoltan Kovacs <giszo.k@gmail.com>
2010-03-09 07:06:22 +0000

    The irc_client application compiles again.

--- source/applications/network/irc_client/ui/ui.c
+++ source/applications/network/irc_client/ui/ui.c
@@ -1,6 +1,7 @@
 /* IRC client
  *
- * Copyright (c) 2009 Zoltan Kovacs, Kornel Csernai
+ * Copyright (c) 2009, 2010 Zoltan Kovacs
+ * Copyright (c) 2009 Kornel Csernai
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of version 2 of the GNU General Public License
@@ -351,5 +352,6 @@ int init_ui( void ) {
 }
 
 int destroy_ui( void ) {
+    endwin();
     return 0;
 }
