cc567082f7b0a64b69ef9911939728505f153161
Zoltan Kovacs <giszo.k@gmail.com>
2010-10-21 11:44:16 +0000

    Header file update.

--- source/include/assert.h
+++ source/include/assert.h
@@ -1,6 +1,6 @@
 /* yaosp C library
  *
- * Copyright (c) 2009 Zoltan Kovacs
+ * Copyright (c) 2009, 2010 Zoltan Kovacs
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of version 2 of the GNU General Public License
@@ -35,10 +35,18 @@
 #include <stdlib.h>
 #include <yaosp/debug.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 static inline void __assert_fail( const char* expr, const char* file, int line ) {
     printf( "Assertion (%s) failed at %s:%d\n", expr, file, line );
     dbprintf( "Assertion (%s) failed at %s:%d\n", expr, file, line );
     abort();
 }
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _ASSERT_H_ */
